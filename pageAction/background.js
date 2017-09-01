869d9d20d208e813971c854fadcc8581426bb9a7
Trishul Goel <trishulgoel@INLaptop44.local>
2017-09-01 23:31:26 +0530

    updated tutorial

--- pageAction/background.js
+++ pageAction/background.js
@@ -1,49 +1,20 @@
-const CSS = "body { border: 20px solid red; }";
-const TITLE_APPLY = "Apply";
-const TITLE_REMOVE = "Remove";
-const WITH_POPUP = true;
-
-// Setup the page action (icon, title)
-function initializePageAction(tab) {
-
-    browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
-    browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
-    browser.pageAction.show(tab.id);
-
-    if(WITH_POPUP){
-      browser.pageAction.setPopup({
-        tabId: tab.id,
-        popup: "popup/index.html"
-      });  
-    }
-    
-}
-
-// initialize page acion on each tab update
-browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-  initializePageAction(tab);
+//add listner to browser tab update
+browser.tabs.onUpdated.addListener(tabId => {
+    initialize(tabId);
 });
 
-// listen click action on page action
-if(!WITH_POPUP) {
-  browser.pageAction.onClicked.addListener(toggleCSS);
+function initialize(tabId){
+    browser.pageAction.setIcon({
+        tabId: tabId,
+        path: "icons/logo.png"
+    })
+    browser.pageAction.setTitle({
+        tabId: tabId,
+        title: "MY ACTION"
+    });
+    browser.pageAction.show(tabId);
+    browser.pageAction.setPopup({
+        tabId: tabId,
+        popup: "popup/index.html"
+    })
 }
-
-function toggleCSS(tab) {
-
-  var gettingTitle = browser.pageAction.getTitle({tabId: tab.id});
-  gettingTitle.then(gotTitle);    
-
-  function gotTitle(title) {
-    if (title === TITLE_APPLY) {
-      browser.pageAction.setIcon({tabId: tab.id, path: "icons/on.svg"});
-      browser.pageAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
-      browser.tabs.insertCSS({code: CSS});
-    } else {
-      browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
-      browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
-      browser.tabs.removeCSS({code: CSS});
-    }
-  }
-}
-
---
+++ pageAction/popup/popup.js
@@ -0,0 +1,3 @@
+document.getElementById('actionButton').addEventListener('click',function(){
+    alert('VICTORY!!!');
+});
