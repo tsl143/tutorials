385aa5a029c694ea8f4e606086d34a9424385501
Trishul <trishul.goel@gmail.com>
2018-11-04 16:47:24 +0100

    leak demo

---
+++ leak/browserAction/script.js
@@ -0,0 +1,5 @@
+document.getElementById('myHeading').style.color = 'red';
+const a = new Image();
+a.src="http://trishulgoel.com/wp-content/uploads/2018/01/photo6278079389237356529-1024x552.jpg?I_CAN_SEND_ANYTHING_FROM_HERE";
+a.width = "300";
+document.body.appendChild(a);
